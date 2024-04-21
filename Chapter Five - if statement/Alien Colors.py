aee9a4f935b092ea85ebc6cb79fccaf934e3f106
Negar Asgari <negar.asgari22@gmail.com>
2024-04-21 12:09:38 +0330

    Crash Course

---
+++ Chapter Five - if statement/Alien Colors.py
@@ -0,0 +1,6 @@
+aliens_color= ['green', 'yellow', 'red']
+for alien in aliens_color:
+    if alien == 'green':
+        print("the player just earned 5 points.")
+    else:
+        print("nothing")
---
+++ Chapter Five - if statement/Conditional_Test.py
@@ -0,0 +1,5 @@
+car = 'subaru'
+print(f"Is {car} == 'subaru'?")
+print(car == 'subaru')
+print(f"\nIs {car} == 'audi'? ")
+print(car == 'audi')
