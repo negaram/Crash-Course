09f17366302d968fd3eeac10fac35a0c2692ba27
Negar Asgari <negar.asgari22@gmail.com>
2024-05-22 09:55:21 +0330

    crash course

---
+++ Chapter Eight- Function/pets.py
@@ -0,0 +1,63 @@
+def describe_pet(animal_type, pet_name):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster', 'harry')
+
+
+print("------------------------------")
+
+
+def describe_pet(animal_type, pet_name):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('hamster', 'harry')
+describe_pet('dog', 'willie')
+
+
+
+print("------------------------------")
+
+
+def describe_pet(animal_type, pet_name):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('harry', 'hamster')
+
+
+
+print("-----------------")
+def describe_pet(animal_type, pet_name):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet(animal_type='hamster', pet_name='harry')
+
+
+
+print("-----------------------------")
+
+def describe_pet(pet_name, animal_type='dog'):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet(pet_name='willie')
+
+print("-----------------------------")
+
+def describe_pet(pet_name="charlli", animal_type='dog'):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet('willie')
+
+
+
+print("-------------------------")
+def describe_pet(animal_type, pet_name):
+ """Display information about a pet."""
+ print(f"\nI have a {animal_type}.")
+ print(f"My {animal_type}'s name is {pet_name.title()}.")
+describe_pet()
